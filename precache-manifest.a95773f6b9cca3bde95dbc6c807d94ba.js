e27f4ffcc9659b4daeb8b6cc8f18dcfa8bf0dafa
Bryan Acuna <bryanacuna1994@icloud.com>
2020-02-26 18:35:45 -0500

    Updates

---
+++ precache-manifest.a95773f6b9cca3bde95dbc6c807d94ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1a7a61497b0d582a1ab28314cf9e50",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "120ad4869dffe026ec79",
+    "url": "/monsters/static/css/main.ba4b6f05.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/monsters/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120ad4869dffe026ec79",
+    "url": "/monsters/static/js/main.d6cc4a63.chunk.js"
+  },
+  {
+    "revision": "16b38dd0445323191b24",
+    "url": "/monsters/static/js/runtime-main.b51bee9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
